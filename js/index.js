bf7396c5ad8fbf4ea36675355fdeef07c64cd9ee
Joao Nardini <jaromaniello@gmail.com>
2019-08-21 15:59:50 -0300

    Added a function that creates a circle

--- js/index.js
+++ js/index.js
@@ -1,21 +1,33 @@
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
-var x = 100;
-
+let x = 100;
+let y = 200;
 function initCanvas() {
-    ctx.rect(x, 10, 50, 50);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.rect(x, y, 50, 50);
+    ctx.closePath();
+    createCircle(50,50,30);
+
     ctx.stroke();
+
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function update() {
     x += 5;
+    y += 2
     initCanvas();
 }
 
 function loop() {
     window.requestAnimationFrame(loop, canvas);
     update();
+}
+
+function createRect(){}
+function createCircle(posX,posY,raio){
+    ctx.arc(posX,posY,raio,0,2*Math.PI);
+    ctx.stroke();
 }
 
 loop();
