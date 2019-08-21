0031dc8eac28e7ed8008c7723434fb8099e52282
alissongp <alisson.paula@neppoti.com.br>
2019-08-21 15:43:06 -0300

    chupa

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
-var canvas = document.querySelector("canvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+var x = 100;
 
+function initCanvas() {
+    ctx.rect(x, 10, 50, 50);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.stroke();
+}
+
+function update() {
+    x += 5;
+    initCanvas();
+}
+
+function loop() {
+    window.requestAnimationFrame(loop, canvas);
+    update();
+}
+
+loop();
