6f779a38b24389ea3a15b19f102aa58c0b6e7c5e
alissongp <alisson.paula@neppoti.com.br>
2019-08-21 16:01:39 -0300

    editing css

--- js/index.js
+++ js/index.js
@@ -2,15 +2,8 @@ var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 var x = 100;
 
-function initCanvas() {
-    ctx.rect(x, 10, 50, 50);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.stroke();
-}
-
 function update() {
-    x += 5;
-    initCanvas();
+    
 }
 
 function loop() {
