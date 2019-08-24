dde9910945e2d0b73049e368f343e3ddc261c525
Joao Nardini <jaromaniello@gmail.com>
2019-08-24 20:30:56 -0300

    Adicionei movimento à peça basica do tetris (1x4)

--- js/index.js
+++ js/index.js
@@ -1,33 +1,66 @@
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 let x = 100;
-let y = 200;
-function initCanvas() {
-    ctx.beginPath();
-    ctx.rect(x, y, 50, 50);
-    ctx.closePath();
-    createCircle(50,50,30);
-
-    ctx.stroke();
-
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
+var y = 200;
+let cx = 125;
+var i = 0;
+var stat = 0;
+var somador = 0;
 function update() {
-    x += 5;
-    y += 2
-    initCanvas();
+if(i!=1){
+    for(;i<4;i++){
+        createRect(x, y, 30, 30);
+        x+= 30;
+}
+}
+i = 1;
+x= 100 + somador;
 }
 
 function loop() {
     window.requestAnimationFrame(loop, canvas);
+    inicializa();
     update();
 }
 
-function createRect(){}
-function createCircle(posX,posY,raio){
-    ctx.arc(posX,posY,raio,0,2*Math.PI);
+function createRect(x, y, lx, ly) {
+    ctx.beginPath();
+    ctx.rect(x, y, lx, ly);
+    ctx.stroke();
+}
+function createCircle(posX, posY, raio) {
+    ctx.beginPath();
+    ctx.arc(posX, posY, raio, 0, 2 * Math.PI);
     ctx.stroke();
 }
 
+function mexer(event){
+    y += 10;
+    console.log(y);
+    
+}
+
+function inicializa(){
+    document.addEventListener('keydown', pegaTecla);
+}
+
+function pegaTecla(){
+  var tecla = event.keyCode;
+  if(tecla == "40"){
+  y += 12;
+  i = 0;}
+ if(tecla == "39"){
+ somador += 12
+ i = 0;
+}
+if(tecla == "37"){
+    somador -= 12
+    i = 0;
+   }
+
+if(tecla == "38"){
+    y -= 12
+    i = 0;
+   }
+}
 loop();
