56e79fb2ba81be49f1d85aae1f84036e48d702dc
alissongp <alisson.paula@neppoti.com.br>
2019-08-24 22:12:52 -0300

    refactoring

--- js/index.js
+++ js/index.js
@@ -11,92 +11,79 @@ my_gradient.addColorStop(0, "black");
 my_gradient.addColorStop(1, "red");
 
 function update() {
-<<<<<<< HEAD
     ctx.lineWidth = 1;
     ctx.strokeRect(0, 25, canvas.width, 0);
-=======
-if(i!=1){
-    for(;i<4;i++){
-        createRect(x, y, 60, 60);
-        ctx.fillStyle = my_gradient;
-        ctx.fillRect(x, y, 60, 60);
-        x+= 60;
-}
-}
-i = 1;
-x= 100 + somador;
->>>>>>> 0d1ae08c41690d546eec886cbc77bc0a46c305a1
-}
+    if (i != 1) {
+        for (; i < 4; i++) {
+            createRect(x, y, 60, 60);
+            ctx.fillStyle = my_gradient;
+            ctx.fillRect(x, y, 60, 60);
+            x += 60;
+        }
+    }
+    i = 1;
+    x = 100 + somador;
 
-function loop() {
-    window.requestAnimationFrame(loop, canvas);
-    inicializa();
-    update();
-  
-}
+    function loop() {
+        window.requestAnimationFrame(loop, canvas);
+        inicializa();
+        update();
 
-<<<<<<< HEAD
-eae(evt) {
-=======
-function createRect(x, y, lx, ly) {
-    ctx.beginPath();
-    ctx.rect(x, y, lx, ly);
-    ctx.stroke();
-}
-function createCircle(posX, posY, raio) {
-    ctx.beginPath();
-    ctx.arc(posX, posY, raio, 0, 2 * Math.PI);
-    ctx.stroke();
-}
+    }
 
-function mexer(event){
-    y += 10;
-    console.log(y);
-    
-}
+    function createRect(x, y, lx, ly) {
+        ctx.beginPath();
+        ctx.rect(x, y, lx, ly);
+        ctx.stroke();
+    }
+    function createCircle(posX, posY, raio) {
+        ctx.beginPath();
+        ctx.arc(posX, posY, raio, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
 
-function inicializa(){
-    document.addEventListener('keydown', pegaTecla);
-}
+    function mexer(event) {
+        y += 10;
+        console.log(y);
+    }
 
-function clear(ctx) {
-    ctx.clearRect(0, 0,canvas.height,canvas.width);
-}
+    function inicializa() {
+        document.addEventListener('keydown', pegaTecla);
+    }
 
-function pegaTecla(){
-  var tecla = event.keyCode;
-  if(tecla == "40"){
-  y += 12;
-  i = 0;
-  clear(ctx);
- }
- if(tecla == "39"){
- somador += 12
- i = 0;
- clear(ctx);
-  }
-if(tecla == "37"){
-    somador -= 12
-    i = 0;
-    clear(ctx);
-   }
- 
-if(tecla == "38"){
-    y -= 12
-    i = 0;
-    clear(ctx);
-   }
-   
-}
+    function clear(ctx) {
+        ctx.clearRect(0, 0, canvas.height, canvas.width);
+    }
 
-<<<<<<< HEAD
-function clear() {
-    clearRect(0, 0,canvas.height,canvas.width);
->>>>>>> 0d1ae08c41690d546eec886cbc77bc0a46c305a1
-}
-=======
+    function pegaTecla() {
+        var tecla = event.keyCode;
+        if (tecla == "40") {
+            y += 12;
+            i = 0;
+            clear(ctx);
+        }
+        if (tecla == "39") {
+            somador += 12
+            i = 0;
+            clear(ctx);
+        }
+        if (tecla == "37") {
+            somador -= 12
+            i = 0;
+            clear(ctx);
+        }
 
->>>>>>> f4118c9644b19f23cff0e666072ec89c0e564f6a
+        if (tecla == "38") {
+            y -= 12
+            i = 0;
+            clear(ctx);
+        }
 
-loop();
-addEventListener('keyup', eae());
+    }
+
+    function clear() {
+        clearRect(0, 0, canvas.height, canvas.width);
+    }
+
+    loop();
+    addEventListener('keyup', eae());
